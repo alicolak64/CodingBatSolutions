ef31d0802c8790256bdde17afda19fd452f4f788
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-07-28 11:28:35 +0300

    Added Warmup-1 > startHi CodingBat Solution
    https://codingbat.com/prob/p191022

---
+++ Warmup-1/startHi.java
@@ -0,0 +1,13 @@
+public class startHi {
+
+	public boolean startHi(String str) {
+
+  		if( str.length() < 2 ) 
+  			return false ;
+
+  		return str.substring( 0 , 2 ).equals("hi"); 
+  		
+	}
+
+
+}
