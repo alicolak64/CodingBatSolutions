c13594ea23260a6b9b7966980a78e2b9342ebe55
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-03 09:48:55 +0300

    Added Warmup-1 > monkeyTrouble CodingBat Solution
    https://codingbat.com/prob/p181646

---
+++ Warmup-1/monkeyTrouble.java
@@ -0,0 +1,13 @@
+public class monkeyTrouble {
+
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+	  if( ( aSmile && bSmile ) || ( !aSmile && !bSmile ) ) 
+	    return true ;
+	  else 
+	  	return false ;
+	  
+	}
+
+
+}
