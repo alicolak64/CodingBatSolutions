94fdb870b36e7077b3af20900b7c3aa2b841aede
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-06 17:43:16 +0300

    Added Warmup-1 > loneTeen CodingBat Solution
    https://codingbat.com/prob/p165701

---
+++ Warmup-1/loneTeen.java
@@ -0,0 +1,10 @@
+public class loneTeen {
+
+	public boolean loneTeen(int a, int b) {
+
+  		return ( ( a >= 13 && a <= 19 ) &&  !( b >= 13 && b <= 19 ) ) || ( (b >= 13 && b <= 19 ) && !( a >= 13 && a <= 19 ) ) ;
+	
+	}
+
+
+}
