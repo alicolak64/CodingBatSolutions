260227345e60125324df8193f4584372eb5f4d71
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-15 11:42:43 +0300

    Added Warmup-1 > stringTimes CodingBat Solution
    https://codingbat.com/prob/p142270

---
+++ Warmup-2/stringTimes.java
@@ -0,0 +1,14 @@
+public class stringTimes {
+
+	public String stringTimes(String str, int n) {
+
+  		String returnStr = "" ;
+  		
+  		for ( int i = 0 ; i < n ; i++ ) {
+    		returnStr += str;
+  		}
+
+  		return returnStr;
+
+	}
+}
