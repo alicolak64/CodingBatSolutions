c78578e4fc0d6e4648b3adefdca06c5de729f948
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-22 09:46:51 +0300

    Added Warmup-2 > countXX CodingBat Solution
    https://codingbat.com/prob/p194667

---
+++ Warmup-2/countXX.java
@@ -0,0 +1,16 @@
+public class countXX {
+
+    int countXX ( String str ) {
+
+        int counter = 0;
+
+        for (int i = 0; i < str.length() - 1; i++) 
+            if (str.substring(i, i + 2).equals("xx")) 
+                counter++;
+            
+
+        return counter;
+
+    }
+
+}
