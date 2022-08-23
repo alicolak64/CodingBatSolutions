bf3db6fbfd51b381720bde2186b92b39025cf1f8
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-23 10:22:54 +0300

    Added Warmup-2 > array667 CodingBat Solution
    https://codingbat.com/prob/p110019

---
+++ Warmup-2/array667.java
@@ -0,0 +1,19 @@
+public class array667 {
+
+    public int array667(int[] nums) {
+
+        int counter = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ( nums[i + 1] == 7 )
+                counter++;
+            if ( ( nums[i] == 6 ) && ( nums[i + 1] == 6 ) ) 
+                counter++;
+            
+        }
+
+        return counter;
+
+    }
+
+}
