d90de6da86ad697e62c34b3c5af80810feb4b3bd
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-10 01:39:52 +0300

    Added Logic-1 > teaParty CodingBat Solution
    https://codingbat.com/prob/p177181

---
+++ Logic-1/teaParty.java
@@ -0,0 +1,16 @@
+public class teaParty {
+
+    public int teaParty (int tea, int candy) {
+
+        if ( tea >= 5 && candy >= 5 ){
+          if ( ( tea / candy >= 2 ) || ( candy / tea >= 2 ) ) 
+            return 2;
+          else 
+            return 1;
+        } else
+            return 0;
+
+      }
+      
+
+}
