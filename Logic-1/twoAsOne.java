3f427c76ebc2888bf7a3584c99db3f09b57b3e4b
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-10 01:41:27 +0300

    Added Logic-1 > twoAsOne CodingBat Solution
    https://codingbat.com/prob/p113261

---
+++ Logic-1/twoAsOne.java
@@ -0,0 +1,12 @@
+public class twoAsOne {
+
+    public boolean twoAsOne ( int a, int b, int c ) {
+
+        double number = (double)(a+b+c) / 2;
+
+        return number == a || number == b || number == c ;
+    
+    }
+      
+
+}
