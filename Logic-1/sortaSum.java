7a6713fa5e073e7061eb816121d57025fe696b38
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-11 14:35:25 +0300

    Added Logic-1 > sortaSum CodingBat Solution
    https://codingbat.com/prob/p183071

---
+++ Logic-1/sortaSum.java
@@ -0,0 +1,15 @@
+public class sortaSum {
+
+    public int sortaSum(int a, int b) {
+
+        int sum = a + b ;
+
+        if ( sum > 9 && sum < 20 ) 
+            return 20;
+        else 
+            return sum;
+            
+    }
+      
+
+}
