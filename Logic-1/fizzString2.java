2264c4cb23ed72f8f24dfaccfb37be3392c5b663
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-14 00:29:55 +0300

    Added Logic-1 > fizzString2 CodingBat Solution
    https://codingbat.com/prob/p115243

---
+++ Logic-1/fizzString2.java
@@ -0,0 +1,18 @@
+public class fizzString2 {
+
+    public String fizzString2 (int n) {
+        
+        if ( n % 3 == 0 || n % 5 == 0 ) {
+          if ( n % 3 == 0 && n % 5 == 0 ) 
+            return "FizzBuzz!";
+          else if ( n % 3 == 0 ) 
+            return "Fizz!";
+          else 
+            return "Buzz!";
+        } 
+        else
+            return n+"!";
+    
+    }  
+
+}
