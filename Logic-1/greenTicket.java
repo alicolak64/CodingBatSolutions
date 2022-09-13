e79d27c4da6baeaf81e01e7cdbf7700344cc7200
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-14 00:34:14 +0300

    Added Logic-1 > greenTicket CodingBat Solution
    https://codingbat.com/prob/p120633

---
+++ Logic-1/greenTicket.java
@@ -0,0 +1,14 @@
+public class greenTicket {
+
+    public int greenTicket(int a, int b, int c) {
+
+        if ( a == b && b == c )  
+            return 20;
+        else if ( a == b || b == c || a == c) 
+            return 10;
+        else 
+            return 0;
+    
+    }
+      
+}
